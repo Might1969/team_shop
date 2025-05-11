155bfe0ad3df91e9ea62314478dc5c722ad37948
dc22842 <1969203751@qq.com>
2025-05-11 20:45:32 +0800

    1.1.1
    fix email

--- project/components/header.php
+++ project/components/header.php
@@ -8,14 +8,12 @@
          <a href="add_product.php">add product</a>
          <a href="view_products.php">view products</a>
          <a href="orders.php">my orders</a>
-             <?php
-                $count_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
-                $count_cart_items->execute([$user_id]);
-                $total_cart_items = $count_cart_items->rowCount();
-             ?>
 
              <?php 
                  if(isset($_SESSION['id'])){
+                  $count_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+                  $count_cart_items->execute([$user_id]);
+                  $total_cart_items = $count_cart_items->rowCount();
              ?>
                <a href="shopping_cart.php" class="cart-btn">cart<span><?= $total_cart_items; ?></span></a>
              <?php
