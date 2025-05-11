fd2a787635b8fe99f7025743bcd67b7826c5341a
dc22842 <1969203751@qq.com>
2025-05-11 21:17:11 +0800

    1.1.5

--- createTable.sql
+++ createTable.sql
@@ -64,6 +64,10 @@ CREATE TABLE `profiles` (
   `users_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+CREATE TABLE `worker` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 ALTER TABLE `profiles`
   ADD PRIMARY KEY (`profiles_id`),
   ADD KEY `users_id` (`users_id`);
--- project/components/header.php
+++ project/components/header.php
@@ -2,12 +2,10 @@
 <header class="header">
 	<?php include_once 'components/connect.php'; ?>
    <section class="flex">
-      <a href="index.php" class="logo">Logo</a>
+      <a href="index.php" class="logo"><img src="img/logo.png"></a>
 
       <nav class="navbar">
-         <a href="add_product.php">add product</a>
          <a href="view_products.php">view products</a>
-         <a href="orders.php">my orders</a>
 
              <?php 
                  if(isset($_SESSION['id'])){
@@ -15,12 +13,29 @@
                   $count_cart_items->execute([$user_id]);
                   $total_cart_items = $count_cart_items->rowCount();
              ?>
+               <a href="orders.php">my orders</a>
                <a href="shopping_cart.php" class="cart-btn">cart<span><?= $total_cart_items; ?></span></a>
              <?php
                  }else{
              ?>
              <?php }?>
+             <?php
+               if(isset($_SESSION['id'])){
+                     $is_worker = $conn->prepare("SELECT * FROM `worker` WHERE id = ?");
+                     $is_worker->execute([$user_id]);
+                     $is_worker = $count_cart_items->rowCount();
+                     if($is_worker>0){
+                        echo'<a href="add_product.php">add product</a>';
+                     }
+                     else{
+                        $count_cart_items = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+                        $count_cart_items->execute([$user_id]);
+                        $total_cart_items = $count_cart_items->rowCount();
 
+                        
+                     }
+                  }
+               ?>
              <?php 
                  if(isset($_SESSION['id'])){
              ?>
