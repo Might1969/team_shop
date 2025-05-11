d43a0dbb6ae9c5e86280d363d0fb6ad6a1325faf
dc22842 <1969203751@qq.com>
2025-05-11 20:45:55 +0800

    1.1.3
    1

--- ProjectAssgn-Ind-PartB-02/footer.php
+++
@@ -1,7 +0,0 @@
-    </div>
-  </body>
-<footer>
-<h2>DC228428 Zhang JiaQi</h2>
-</footer>
-</html>
-<script src="js/script.js"></script>
--- ProjectAssgn-Ind-PartB-05/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-<h2>DC228428 Zhang JiaQi</h2>
-</footer>
-
-</body>
-</html>
